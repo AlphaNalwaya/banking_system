d30cd5290d6472cdf7a4ee7527a9d803e3b131ab
AlphaNalwaya <alphanalwaya91@gmail.com>
2018-10-06 21:30:13 +0530

    issue fixed

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,5 @@ class Loan < ApplicationRecord
   belongs_to :user
   validates :loan_type, :amount, :interest_rate, presence: true
   validates :amount, :interest_rate,  numericality: true
+  validates :type, inclusion: { in: %w(Home_Loan Car_Loan Education_Loan PersonaL_Loan Business_Loan)}
 end
